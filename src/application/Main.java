70fe09ff85558fb868d004ef557451aea39191c0
TannerTX <tanner_bibb@yahoo.com>
2021-10-27 20:54:00 -0500

    Initialized JavaFX Project Packages

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
+			//
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
