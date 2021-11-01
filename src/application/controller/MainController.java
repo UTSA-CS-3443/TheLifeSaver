600b53421b1e5c022c245ff5fa6d01b98256e164
crocharimsa <cjnewco@icloud.com>
2021-11-01 16:38:18 -0500

    controller package skeleton

---
+++ src/application/controller/MainController.java
@@ -0,0 +1,13 @@
+package application.controller;
+
+import javafx.event.Event;
+
+public class MainController implements javafx.event.EventHandler<Event>{
+
+	@Override
+	public void handle(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
