4b2c4fd4007094d76db181d5c9ae6bcda5ab8a23
cjnewco <cjnewco@icloud.com>
2021-11-02 18:50:54 -0500

    calenderController

--- src/application/controller/CalenderController.java
+++ src/application/controller/CalenderController.java
@@ -1,5 +1,22 @@
 package application.controller;
 
-public class CalenderController {
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
+import javafx.fxml.Initializable;
+
+public class CalenderController implements Initializable{
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		//load in the data of the current month
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");  
+		LocalDateTime now = LocalDateTime.now();
+		String month = now.format(dtf).split("/")[0];
+		
+		
+	}
 }
