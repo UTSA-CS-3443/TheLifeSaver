ed7c489b3b893459c7069b8587c12dc82cc3b6bc
Tanner <tanner_bibb@yahoo.com>
2021-11-03 02:51:23 -0500

    Added event handler for swapping scenes

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -1,12 +1,65 @@
 package application.controller;
 
-import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.fxml.*;
 
-public class MainController implements javafx.event.EventHandler<Event>{
+public class MainController implements EventHandler<ActionEvent> {
 
+	@FXML
+	private AnchorPane rootPane;
+	
+	// Variable names will be btn1-4 until we actually finish the fxmls
+	@FXML
+	private Button btn1;
+	
+	@FXML
+	private Button btn2;
+	
+	@FXML
+	private Button btn3;
+	
+	@FXML
+	private Button btn4;
+	
 	@Override
-	public void handle(Event event) {
-		// TODO Auto-generated method stub
+	public void handle(ActionEvent event) {
+		
+		Button tmp = (Button) event.getSource();
+		AnchorPane newPane;
+		
+		try {
+		
+			switch(tmp.getText()) {
+			// **We can edit the case names if needed**
+			
+			case "Daily": 
+				newPane = FXMLLoader.load(getClass().getClassLoader().getResource("view/Daily.fxml"));
+				rootPane.getChildren().setAll(newPane);
+				break;
+				
+			case "Monthly": 
+				newPane = FXMLLoader.load(getClass().getClassLoader().getResource("view/Monthly.fxml"));
+				rootPane.getChildren().setAll(newPane);
+				break;
+				
+			case "Reminders":
+				newPane = FXMLLoader.load(getClass().getClassLoader().getResource("view/Reminders.fxml"));
+				rootPane.getChildren().setAll(newPane);
+				break;
+				
+			case "Weekly": 
+				newPane = FXMLLoader.load(getClass().getClassLoader().getResource("view/Weekly.fxml"));
+				rootPane.getChildren().setAll(newPane);
+				break;
+			}
+			
+			
+		}
+		catch(Exception e) {e.printStackTrace();}
+		
 		
 	}
 
