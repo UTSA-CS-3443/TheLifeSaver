9819bf92788beef0583a8892d305c7d286565e21
ajb733 <90349646+ajb733@users.noreply.github.com>
2021-11-07 14:26:29 -0600

    working on showing the events in week

--- src/application/controller/WeeklyController.java
+++ src/application/controller/WeeklyController.java
@@ -10,21 +10,22 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.ResourceBundle;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import static java.time.temporal.TemporalAdjusters.nextOrSame;
-import static java.time.temporal.TemporalAdjusters.previousOrSame;
-import static java.time.DayOfWeek.MONDAY;
+
 
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class WeeklyController implements EventHandler<ActionEvent>, Initializable {
 
@@ -32,32 +33,24 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 	@FXML Button backButton;
 	@FXML GridPane weeklyGrid;
 	@FXML Label MonthName;
+    @FXML private Button monButton;
+    @FXML private Button friButton;
+    @FXML private Button satButton;
+    @FXML private Button thursButton;
+    @FXML private Button wedButton;
+    @FXML private Button sunButton;
+    @FXML private Button tuesButton;
     @FXML
-    private Button monButton;
-    @FXML
-    private Button friButton;
-
-    @FXML
-    private Button satButton;
-
-    @FXML
-    private Button thursButton;
-
-    @FXML
-    private Button wedButton;
-
-    @FXML
-    private Button sunButton;
-
-    @FXML
-    private Button tuesButton;
+    private Label testLabel;
  
-	
+	@Override
     public void initialize(URL location, ResourceBundle resources){
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");  
 		LocalDateTime now = LocalDateTime.now();
 		String month = now.format(dtf).split("/")[0];
+		String day = now.format(dtf).split("/")[1];
 		
+		//ik its alot, but reads in the current date and only shows that weeks dates
 	    LocalDateTime sun=now;
 		LocalDateTime mon = now;
 		LocalDateTime tue = now;
@@ -65,6 +58,7 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 		LocalDateTime thurs = now;
 		LocalDateTime fri = now;
 		LocalDateTime end = now;
+		
 		switch(now.getDayOfWeek().toString()) {
 		case "SUNDAY":
 			sun.getDayOfWeek();
@@ -83,6 +77,7 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 			thurs=thurs.plusDays(3);
 			fri=fri.plusDays(4);
 			end=end.plusDays(5);
+			break;
 		case "TUESDAY":
 			sun=sun.minusDays(2);
 			mon=mon.minusDays(1);
@@ -91,6 +86,7 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 			thurs=thurs.plusDays(2);
 			fri=fri.plusDays(3);
 			end=end.plusDays(4);
+			break;
 		case "WEDNESDAY":
 			sun=sun.minusDays(3);
 			mon=mon.minusDays(2);
@@ -99,6 +95,7 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 			thurs=thurs.plusDays(1);
 			fri=fri.plusDays(2);
 			end=end.plusDays(3);
+			break;
 		case "THURSDAY":
 			sun=sun.minusDays(4);
 			mon=mon.minusDays(3);
@@ -107,6 +104,7 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 			thurs.getDayOfWeek();
 			fri=fri.plusDays(1);
 			end=end.plusDays(2);
+			break;
 		case "FRIDAY":
 			sun=sun.minusDays(5);
 			mon=mon.minusDays(4);
@@ -115,6 +113,7 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 			thurs=thurs.minusDays(1);
 			fri.getDayOfWeek();
 			end=end.plusDays(1);
+			break;
 		case "SATURDAY":
 			sun=sun.minusDays(6);
 			mon=mon.minusDays(5);
@@ -123,16 +122,10 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 			thurs=thurs.minusDays(2);
 			fri=fri.minusDays(1);
 			end.getDayOfWeek();
+			break;
+		default:
+			break;
 		}
-		
-		
-		
-	    
-	    
-
-	    System.out.println("Today: " + now);
-	    System.out.println("Start of the Week: " + sun);
-	    System.out.println("End of the Week: " + end);
 	    
 	    String suns= sun.format(dtf).split("/")[1];
 	    sunButton.setText(suns);
@@ -149,17 +142,41 @@ public class WeeklyController implements EventHandler<ActionEvent>, Initializabl
 	    String fris= fri.format(dtf).split("/")[1];
 	    friButton.setText(fris);
 	    
-	
-
-	    
-		MonthName.setText(Month.of(Integer.parseInt(month)).name());
+	    String [] days= {suns,mons,tues,weds,thurss,fris,ends};
+        MonthName.setText(Month.of(Integer.parseInt(month)).name());
 		
 		Occation eventlist = new Occation();
 		eventlist.loadEvents("data/monthlyEvents.csv");
-		// Stopped here, coming back later
+		System.out.println(day);
+		
+	   for (String s: days) {
+		   checkEvent(s,eventlist);
+		   
+	   }
+	    
+		
+	
+		
+
 	}
+	//not done
+	public void checkEvent(String day, Occation eventlist) {	
+		for( int i = 0; i < eventlist.getEvents().size(); i++ ) {
+				if(eventlist.getEvents().get(i).getDate().split("/")[1].equals(day)) {
+					String ret= eventlist.getEvents().get(i).getName();
+					String one = eventlist.getEvents().get(i).getDate();
+					String here= ret + " - " + one;
+					testLabel.setText(here);
+					//return here;
+					//System.out.print(eventlist.getEvents().get(i).getName()+" - "+eventlist.getEvents().get(i).getDate() );
+					//System.out.print(" - "+eventlist.getEvents().get(i).convertToStandard());
+					//System.out.println("\n" + eventlist.getEvents().get(i).getNote() + "\n");
+				}
+			}
+		
+		
 	
-	
+	}		
 	@Override
 	public void handle(ActionEvent event) {
 		Button temp = (Button) event.getSource();
