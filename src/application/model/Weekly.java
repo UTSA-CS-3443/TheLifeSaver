7b63cbf0e39bf44e881820e7a04ef6e203ea52bb
ajb733 <90349646+ajb733@users.noreply.github.com>
2021-11-07 10:55:24 -0600

    made methods to go to start of and end of week based on dates. Not done
    made methods to go to start of and end of week based on dates. Not done

--- src/application/model/Weekly.java
+++ src/application/model/Weekly.java
@@ -2,110 +2,43 @@ package application.model;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
+
+
 public class Weekly {
 	
-	//class variable
-	private String month;
-	private int day;
-    private ArrayList<Plan> plans;
-	
-    public Weekly() {
-    	this.month = "";
-    	this.day=0;
-    	this.plans = new ArrayList<Plan>();
-    }
-
-    public Weekly(String month, int day) {
-        this.month = month;
-        this.day = day;
-        this.plans = new ArrayList<Plan>();
-    }
-	
-	
-	//read in file and get the date, create that as a weekly object
-	/*public static Plan loadPlan (String filename) {
-		try {
-			Scanner scan = new Scanner(new File(filename));
-			
-			while(scan.hasNextLine()) {
-				String line= scan.nextLine();
-				String[] tokens = line.split( "," );
-				if(tokens!=null && tokens.length==5) {
-					String date = tokens[0];
-					String time = tokens[1];
-					String name = tokens[2];
-					String remind = tokens[3];
-					String note = tokens[4];
-					
-					Plan event = new Plan (date,time,name,remind,note);
-				}
-				
-			}
+	public Weekly() {
 		
-	}catch (IOException e) {
-		e.printStackTrace();
-	}
-		return null;
-
-	}
-	public String toString() {
-		
-	}*/
-
-	/**
-	 * @return the plans
-	 */
-	public ArrayList<Plan> getPlans() {
-		return plans;
-	}
-
-
-
-	/**
-	 * @param plans the plans to set
-	 */
-	public void setPlans(ArrayList<Plan> plans) {
-		this.plans = plans;
-	}
-
-
-
-	/**
-	 * @return the month
-	 */
-	public String getMonth() {
-		return month;
-	}
-
-
-
-	/**
-	 * @param month the month to set
-	 */
-	public void setMonth(String month) {
-		this.month = month;
-	}
-
-
-
-	/**
-	 * @return the day
-	 */
-	public int getDay() {
-		return day;
-	}
-
-
-
-	/**
-	 * @param day the day to set
-	 */
-	public void setDay(int day) {
-		this.day = day;
 	}
 	
-	//
+	//method to find start of week
+	public static Date getWeekStartDate(Date date) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		//goes backward to monday, to start week
+		while (c.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+	        c.add(Calendar.DATE, -1);
+	    }
+		return c.getTime();
+	}
+	
+	//method to find end of week
+	public static Date getWeekEndDate(Date date) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		//go forward until it gets to the monday
+		while (c.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+	        c.add(Calendar.DATE, 1);
+	    }
+		//go back one, since the loop above goes to monday
+		c.add(Calendar.DATE, -1);
+		return c.getTime();
+	}
+	
+	
+
 
 }
