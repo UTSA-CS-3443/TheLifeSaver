9b022afc8bb22ad998a7ebf903acf20bfeffa700
Dont Worry About it <tanner_bibb@yahoo.com>
2021-11-07 20:07:40 -0600

    Added navigation for Monthly to change months

--- src/application/controller/CalenderController.java
+++ src/application/controller/CalenderController.java
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 //import java.util.List;
 import java.util.ResourceBundle;
-import application.model.Occation;
+
 import application.model.*;
 import javafx.collections.FXCollections;
 import javafx.scene.control.TextField;
@@ -21,6 +21,8 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -32,39 +34,43 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class CalenderController implements javafx.event.EventHandler<Event>, Initializable{
+public class CalenderController implements javafx.event.EventHandler<Event>{
 	
 	@FXML AnchorPane rootPane, addEventRoot;
-	@FXML Button backButton, addButton, addEventAdd, addEventX;
+	@FXML Button backButton, addButton, addEventAdd, addEventX, incrementMonth, decrementMonth;
 	@FXML TextField addEventName, addEventTime, addEventNotes;
 	@FXML DatePicker addEventDate;
 	@FXML Label MonthName, addEventError;
 	@FXML GridPane calenderGrid;
+	static String globalMonth;
 	
-//	private ObservableList<ObservableList<>> weeks;
-//	private ObservableList<> day; - 
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	public void initialize(String month, String year) {
 		// TODO Auto-generated method stub
 		//load in the data of the current month
+		if(month.charAt(0) == '0')
+			month = String.valueOf(month.charAt(1));
+			
+		globalMonth = month;
+		
+		if(globalMonth.equals("12"))
+			incrementMonth.setVisible(false);
+		else if(globalMonth.equals("1"))
+			decrementMonth.setVisible(false);
+		
 		addEventRoot.setVisible(false);
 		addEventError.setVisible(false);
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");  
-		LocalDateTime now = LocalDateTime.now();
-		String month = now.format(dtf).split("/")[0];
 		
 		Occation eventlist = new Occation();
 		eventlist.loadEvents("data/monthlyEvents.csv");
 		
 		//find the number of days in the month
-		Calendar c = Calendar.getInstance();
+		Calendar c = new GregorianCalendar(2021, Integer.parseInt(month) - 1, 1);
 		int days = c.getActualMaximum(Calendar.DAY_OF_MONTH);
 		int fdaypos = -1;
 		
 		//find the position in the week we will start the month in
-		// EDIT: Sunday is usually the start of a calendar, so index 
-		switch( LocalDate.of( Integer.parseInt(now.format(dtf).split("/")[2]), Integer.parseInt(now.format(dtf).split("/")[0]), Integer.parseInt("01") ).getDayOfWeek().toString() ){
+		
+		switch( LocalDate.of( Integer.parseInt(year), Integer.parseInt(month) + 1, Integer.parseInt("01") ).getDayOfWeek().toString() ){
 			case "MONDAY":		fdaypos = 1;
 			break;
 			case "TUESDAY": 	fdaypos = 2;
@@ -81,12 +87,6 @@ public class CalenderController implements javafx.event.EventHandler<Event>, Ini
 			default: System.out.println("fdaypos reader is broken");
 		}
 		
-		//find how many rows the calender grid will need
-		
-		int weeks = (fdaypos + days - 1 ) / 7;
-		if((fdaypos + days - 1 ) % 7 != 0 ) {
-			weeks++;
-		}
 		
 		//load month data into the calendar--------------------------------
 		
@@ -95,7 +95,10 @@ public class CalenderController implements javafx.event.EventHandler<Event>, Ini
 		int ct = 0, currDay = 1;
 		boolean flag = false;
 		String filler;
-
+		
+		if(month.length() == 1)
+			month = "0" + month;
+		
 		for (Node node: calenderGrid.getChildren()) {
 			  
 			 // The actual text that'll be put in the cell(s)
@@ -106,11 +109,7 @@ public class CalenderController implements javafx.event.EventHandler<Event>, Ini
 			  if (ct == fdaypos)
 			    flag = true;
 
-			  
-			  // Sorry for the clusterfuck code, this took 4 hours to figure out and I'm slump
-
 			  filler += String.format("%s\n", String.valueOf(currDay)); 
-
 			  
 			  // Adds the event name to the correct date as long as the month and the day match the one on file
 			  for (Plan event: eventlist.getEvents()) 
@@ -123,7 +122,7 @@ public class CalenderController implements javafx.event.EventHandler<Event>, Ini
 			  if (node instanceof Label && flag && currDay <= days) {
 			    ((Label) node).setText(filler);
 			    currDay++;
-			  } else if (node instanceof Label && ct > days)
+			  } else if (node instanceof Label && ct >= days)
 			    ((Label) node).setText("");
 
 			  else if (node instanceof Label && !flag)
@@ -150,26 +149,46 @@ public class CalenderController implements javafx.event.EventHandler<Event>, Ini
 
 	@Override
 	public void handle(Event event) {
-		// TODO handle adding a new event to the calender
-		
-		
-		
-		// TODO handle deleting an event from the calender
-		
-		
-		// TODO handle navigating to other pages
+	
 		Button temp = (Button) event.getSource();
+		FXMLLoader loader;
 		AnchorPane newPane;
 		try {
+			
 			if(temp.equals(backButton)) {
 				newPane = FXMLLoader.load(getClass().getClassLoader().getResource("application/view/Main.fxml"));
 				rootPane.getChildren().setAll(newPane);
 			}	
+			
+			else if(temp.equals(incrementMonth)) {
+			
+				String newMonth = String.valueOf(Integer.parseInt(globalMonth) + 1);
+				
+			    loader = new FXMLLoader(getClass().getClassLoader().getResource("application/view/Monthly.fxml"));
+			    newPane = (AnchorPane) loader.load();
+				rootPane.getChildren().setAll(newPane);
+				
+				CalenderController controller = loader.getController();
+				controller.initialize(newMonth, "2021");
+			}
+			
+			else if(temp.equals(decrementMonth)) {
+				
+				String newMonth = String.valueOf(Integer.parseInt(globalMonth) - 1);
+				
+			    loader = new FXMLLoader(getClass().getClassLoader().getResource("application/view/Monthly.fxml"));
+			    newPane = (AnchorPane) loader.load();
+				rootPane.getChildren().setAll(newPane);
+				
+				CalenderController controller = loader.getController();
+				controller.initialize(newMonth, "2021");
+			}
 		}
 		catch(Exception e) {e.printStackTrace();}
-		// TODO handle navigating to a different month
 		
 	}
+	
+	
 	
 	public void addEventGuiHandler(Event event) {
 		
@@ -203,16 +222,25 @@ public class CalenderController implements javafx.event.EventHandler<Event>, Ini
 				String vals[] = addEventDate.getValue().toString().split("-");
 				String newDate = String.format("%s/%s/%s", vals[1], vals[2], vals[0]);
 				SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-				Date date = format.parse(newDate);
 				
 				// Didn't know if we had a formal method for writing to the data file, but this does the trick
 				FileWriter fileWriter = new FileWriter("data/monthlyEvents.csv", true);
 				PrintWriter printWriter = new PrintWriter(fileWriter);
-				printWriter.println(String.format("%s,%s,%s,F,%s", newDate, addEventTime.getText(), addEventName.getText(), addEventNotes.getText()));
-				printWriter.close();
+				
+				if(addEventNotes.getText().isEmpty())
+					printWriter.println(String.format("%s,%s,%s,F,%s", newDate, addEventTime.getText(), addEventName.getText(), "-"));
 	
-				AnchorPane newPane = FXMLLoader.load(getClass().getClassLoader().getResource("application/view/Monthly.fxml"));
+				else
+					printWriter.println(String.format("%s,%s,%s,F,%s", newDate, addEventTime.getText(), addEventName.getText(), addEventNotes.getText()));
+				
+				printWriter.close();
+				
+				FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("application/view/Monthly.fxml"));
+				AnchorPane newPane = (AnchorPane) loader.load();
 				rootPane.getChildren().setAll(newPane);
+				
+				CalenderController controller = loader.getController();
+				controller.initialize(newDate.split("/")[0], "2021");
 				
 				}catch(Exception e) {e.printStackTrace();}
 			}
