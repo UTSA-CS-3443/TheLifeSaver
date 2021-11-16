c210db2f113dfc3f9aa0e2532fd3e90631a6fec4
jademanriquez <91292552+jademanriquez@users.noreply.github.com>
2021-11-15 21:41:22 -0600

    Update DailyController.java

--- src/application/controller/DailyController.java
+++ src/application/controller/DailyController.java
@@ -8,10 +8,9 @@
  */
 package application.controller;
 
-import application.controller.CalenderController;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -27,11 +26,11 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-//import javafx.scene.control.TextArea;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 public class DailyController implements EventHandler<ActionEvent>, Initializable {
 	
@@ -65,6 +64,11 @@ public class DailyController implements EventHandler<ActionEvent>, Initializable
 
 	private CheckBox[] boxes;
 	
+	/**
+	 * Populates the To-Do list view with our events before the scene fully loads
+	 * @param location, URL
+	 * @param resources, ResourceBundle
+	 */
 		public void initialize(URL location, ResourceBundle resources) {
 			addEventRoot.setVisible(false);
 			addEventError.setVisible(false);
@@ -85,20 +89,24 @@ public class DailyController implements EventHandler<ActionEvent>, Initializable
 				}
 			}
 		}
-		
+		/**
+		 * Handles the CheckBox events- disabling/change to red when checked
+		 * @param event, ActionEvent
+		 */
 		@Override
 		public void handle(ActionEvent event) {
 			for(int i = 0; i < 15; i++) {
 			if(boxes[i].isSelected()) {
+				boxes[i].setTextFill(Color.RED);
 				boxes[i].setDisable(true);
-				//myItems.getItems().remove(i);
-
 				}
-			}
-			
+			}	
 		}
 	
-
+		/**
+		 * Sends the user to a different view, Main view.
+		 * @param event, ActionEvent
+		 */
 		public void goBack(ActionEvent event) {
 			try {
 				AnchorPane root = new AnchorPane();
@@ -115,7 +123,9 @@ public class DailyController implements EventHandler<ActionEvent>, Initializable
 			}
 		}
 		
-
+		/**
+		 *   Sets all the checkBoxes to not visible.
+		 */
 		public void notVisible() {
 			item1.setVisible(false);
 			item2.setVisible(false);
@@ -134,7 +144,10 @@ public class DailyController implements EventHandler<ActionEvent>, Initializable
 			item15.setVisible(false);
 		}	
 		
-
+		/**
+		 * Handles the button press events on the event adder GUI
+		 * @param event
+		 */
 		public void addEventGuiHandler(Event event) {
 		
 			if(event.getSource().equals(addButton)) {
@@ -186,5 +199,5 @@ public class DailyController implements EventHandler<ActionEvent>, Initializable
 				
 			}
 		}
-		
 }
+
